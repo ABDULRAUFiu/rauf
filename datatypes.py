c9141bf24faede3a277d76b74f808a3cef02e108
ABDULRAUFiu <156327312+ABDULRAUFiu@users.noreply.github.com>
2024-01-16 22:02:41 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+# checking out the type of float
+float_1= 7.5
+print(float_1, "is of this type",type(float_1))
+
+#checking out the type of string
+string_1= "Rauf"
+print(string_1, "is of this type",type(string_1))
+
+# checking out the type of list
+list= [1, 2, 3 ,4]
+print(list,"is of this type", type(list))
+
+
+
+print(True or False)
---
+++ task1.py
@@ -0,0 +1,8 @@
+# int numbers;
+# Making an addition of numbers
+numbers=1+2+3+4+5
+print("The sum of the numbers are:", numbers)
+
+#Making an addition of numbers and then divide the result by 3
+num=((23+19+31)/3)
+print("Result is :", num )
